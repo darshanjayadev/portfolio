7ed260bc191ef2a16399e199ace54f393f092033
juzQrios <dj30c.1@gmail.com>
2020-03-27 20:02:46 +0530

    Add social links to header
    Closes #9

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import githubLogo from '../img/logos/github.png'
+import twitterLogo from '../img/logos/twitter.png'
 
 const Header = () => {
   return (
@@ -7,6 +9,16 @@ const Header = () => {
       <h3>Web Designer & Developer</h3>
       <div className="mail">
         <a className="mail-link" href="mailto:dj30c.1@gmail.com">dj30c.1@gmail.com</a>
+      </div>
+      <div className="social-logos">
+      <a className="logo-link" href="https://github.com/juzQrios">
+        {/* https://github.com/logos */}
+        <img src={githubLogo} alt="GitHub" />
+      </a>
+      <a className="logo-link" href="https://twitter.com/DarshanJayadev">
+        {/* https://about.twitter.com/en_us/company/brand-resources.html */}
+        <img src={twitterLogo} alt="Twitter" />
+      </a>
       </div>
     </header>
   );
